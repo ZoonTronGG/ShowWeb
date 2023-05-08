a1424d78df87f2ddcd7436128bd29fe29f3931ff
Titorenko Artyom <unknown@mail.ru>
2023-05-08 23:59:07 +0600

    Added EF and make init Migration

---
+++ ShowWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShowWeb.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
