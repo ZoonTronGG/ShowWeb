a5f1d1aad3f558389029c3354ccc8f86cd6d8ae1
Titorenko Artyom <unknown@mail.ru>
2023-05-24 22:38:58 +0600

    Added attributes MaxLength, Range for Server-Side Validation and Custom Validation. Also used asp-validation-summary, asp-validation-for

--- ShowWeb/Controllers/CategoryController.cs
+++ ShowWeb/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShowWeb.Data;
+using ShowWeb.Models;
 
 namespace ShowWeb.Controllers;
 
@@ -20,5 +21,17 @@ public class CategoryController : Controller
     public IActionResult Create()
     {
         return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
+        }
+        if (!ModelState.IsValid) return View(obj);
+        _db.Categories.Add(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
