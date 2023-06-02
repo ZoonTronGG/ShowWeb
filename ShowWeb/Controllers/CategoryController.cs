1366d44648dc2c9655e405094064e1a718a6ce8f
Titorenko Artyom <unknown@mail.ru>
2023-06-03 00:24:12 +0600

    Added Notification + CRUD for Category

--- ShowWeb/Controllers/CategoryController.cs
+++ ShowWeb/Controllers/CategoryController.cs
@@ -25,13 +25,57 @@ public class CategoryController : Controller
     [HttpPost]
     public IActionResult Create(Category obj)
     {
-        if (obj.Name == obj.DisplayOrder.ToString())
-        {
-            ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
-        }
+        // if (obj.Name == obj.DisplayOrder.ToString())
+        // {
+        //     ModelState.AddModelError("Name", "Name and Display Order cannot be the same");
+        // }
         if (!ModelState.IsValid) return View(obj);
         _db.Categories.Add(obj);
         _db.SaveChanges();
+        TempData["Success"] = "The category has been added successfully";
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id is null or 0)
+        {
+            return NotFound();
+        }
+        var obj = _db.Categories.Find(id);
+        if (obj == null) return NotFound();
+        return View(obj);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category obj)
+    {
+        if (!ModelState.IsValid) return View(obj);
+        _db.Categories.Update(obj);
+        _db.SaveChanges();
+        TempData["Success"] = "The category has been updated successfully";
+        return RedirectToAction("Index");
+    }
+    
+    public IActionResult Delete(int? id)
+    {
+        if (id is null or 0)
+        {
+            return NotFound();
+        }
+        var obj = _db.Categories.Find(id);
+        if (obj == null) return NotFound();
+        return View(obj);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeletePost(int? id)
+    {
+        var category = _db.Categories.Find(id);
+        if (category == null) return NotFound();
+        _db.Categories.Remove(category);
+        _db.SaveChanges();
+        TempData["Success"] = "The category has been deleted successfully";
         return RedirectToAction("Index");
     }
 }
